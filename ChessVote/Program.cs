7a23bb214ed0ff559a9bf25b781bd78479ed55be
Andrew Gerasimovich <byte916@yandex.ru>
2022-06-25 23:10:49 +0300

    Убраны все лишние модули из Startup, добавлен MVC

--- ChessVote/Program.cs
+++ ChessVote/Program.cs
@@ -1,25 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.EnableEndpointRouting = false;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseMvc(routeBuilder =>
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+    routeBuilder.MapRoute(
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
